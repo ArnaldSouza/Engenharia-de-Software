bb327267a3ba4215f617121c98ff9d3dcad38d73
Bruno Targa <brunotarga08@gmail.com>
2024-05-06 12:15:11 -0300

    navigation login

--- Frontend/src/routes/stack.tsx
+++ Frontend/src/routes/stack.tsx
@@ -5,11 +5,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import home from "../screens/home";
 import perfil from "../screens/perfil";
 import criarConta from "../screens/criarConta";
+import Login from '../screens/login';
 
 export default function StackComponent() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>   
+      <Stack.Navigator>
+        <Stack.Screen name ="Login" component={Login} />
         <Stack.Screen name ="home" component={home} />
         <Stack.Screen name ="perfil" component={perfil} />  
         <Stack.Screen name ="criarConta" component={criarConta} />  
